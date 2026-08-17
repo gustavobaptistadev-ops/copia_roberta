1c77f49504552a72faf46a0cc47fe381d3c905d4
gustavobaptistadev-ops <gustavo.baptista.dev@gmail.com>
2026-08-17 08:16:43 -0300

    Delete nina-backend directory

--- nina-backend/app/core/prompts/templates.py
+++
@@ -1,7 +0,0 @@
-NINA_DEFAULT_PRESENTATION_MESSAGE = (
-    "Olá, tudo bem? Sou a Roberta da clínica Vittamed. Qual é o seu nome?"
-)
-
-NINA_CLINIC_LOCATION = (
-    "Estamos localizados nas principais unidades da região de Brasília."
-)
